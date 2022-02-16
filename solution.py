0da15e3c5b48e34ddef8f78807483140cf9def29
desantw <96094125+desantw@users.noreply.github.com>
2022-02-16 17:33:17 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,14 +3,12 @@ from socket import *
 # In order to terminate the program
 import sys
 
-host = '127.0.0.1'
-port = 13331
 
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
     # Fill in start
-    serverSocket.bind(("", port))
+    serverSocket.bind(("127.0.0.1", port))
     
     # Fill in end
 
