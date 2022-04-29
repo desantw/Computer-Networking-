611536663944701b7b0335af8305081b89a4edf3
desantw <96094125+desantw@users.noreply.github.com>
2022-04-29 17:08:08 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -121,7 +121,7 @@ def ping(host, timeout=1):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)
-        val.append(delay)
+        val.append(val)
         #one second
         
 
